9680411f5e93a74cd7bd04d651dcb6a8dcb6b88f
Yi Lin <yilin4@microsoft.com>
2024-11-21 19:34:50 +0800

    taskpane - change sensitive level

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -9,21 +9,69 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    document.getElementById("nonBusiness").onclick = changeToNonBusinessLevel;
+    document.getElementById("public").onclick = changeToPublicLevel;
+    document.getElementById("general").onclick = changeToGeneralLevel;
+    document.getElementById("confidential").onclick = changeToConfidentialLevel;
+    document.getElementById("highConfidential").onclick = changeToHighConfidentialLevel;
   }
 });
 
-export async function run() {
+export async function changeToNonBusinessLevel() {
   return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
+    const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+    header.clear();
+    header.insertParagraph("Non-Business - The data is personal and not business related", "Start");
+    const font = header.font;
+    font.color = "#737173";
 
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+    await context.sync();
+  });
+}
 
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+export async function changeToPublicLevel() {
+  return Word.run(async (context) => {
+    const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+    header.clear();
+    header.insertParagraph("Public - The data is for the public and shareable externally", "Start");
+    const font = header.font;
+    font.color = "#07641d";
+
+    await context.sync();
+  });
+}
+
+export async function changeToGeneralLevel() {
+  return Word.run(async (context) => {
+    const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+    header.clear();
+    header.insertParagraph("General - Business data shared with trusted individuals", "Start");
+    const font = header.font;
+    font.color = "#0177d3";
+
+    await context.sync();
+  });
+}
+
+export async function changeToConfidentialLevel() {
+  return Word.run(async (context) => {
+    const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+    header.clear();
+    header.insertParagraph("Confidential - Sensitive business data shared with trusted individuals", "Start");
+    const font = header.font;
+    font.color = "#ff5c3a";
+
+    await context.sync();
+  });
+}
+
+export async function changeToHighConfidentialLevel() {
+  return Word.run(async (context) => {
+    const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+    header.clear();
+    header.insertParagraph("High Confidential - The data must be secret or in some way highly critical", "Start");
+    const font = header.font;
+    font.color = "#f8334d";
 
     await context.sync();
   });
