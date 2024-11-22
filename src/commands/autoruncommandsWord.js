505cc5f2a10a4213b038d042192143fb92b0db4d
YijunMS <112541209+YijunMS@users.noreply.github.com>
2024-11-22 11:20:51 +0800

    Update autoruncommandsWord.js

--- src/commands/autoruncommandsWord.js
+++ src/commands/autoruncommandsWord.js
@@ -9,21 +9,30 @@ Office.onReady(() => {
   // If needed, Office.js is ready to be called
 });
 
-// async function paragraphChanged(event: Word.ParagraphChangedEventArgs) {
-//   await Word.run(async (context) => {
-//     const paragraph = context.document.body.insertParagraph(
-//      "event detected.",
-//       Word.InsertLocation.end
-//     );
-//     await context.sync();
-//   });
-// }
-
 async function onParagraphChanged(event) {
   Word.run(async (context) => {
-    // let eventContext = context.document.onParagraphChanged.add(paragraphChanged);
-    const paragraph = context.document.body.insertParagraph("Hello", Word.InsertLocation.end);
+    const body = context.document.body;
+    body.load("text");
     await context.sync();
+    if (body.text.length == 0)
+    {
+      const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+      header.clear();
+      header.insertParagraph("Public - The data is for the public and shareable externally", "Start");
+      const font = header.font;
+      font.color = "#07641d";
+
+      await context.sync();
+    }
+    else
+    {
+      const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+      header.clear();
+      header.insertParagraph("High Confidential - The data must be secret or in some way highly critical", "Start");
+      const font = header.font;
+      font.color = "#f8334d";
+      await context.sync();
+    }
   });
 
   // Calling event.completed is required. event.completed lets the platform know that processing has completed.
