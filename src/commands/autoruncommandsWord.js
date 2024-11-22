b4c4a7ef8eca28773b0b24aabe1cf82be26d1805
YijunMS <112541209+YijunMS@users.noreply.github.com>
2024-11-22 13:48:20 +0800

    Update autoruncommandsWord.js

--- src/commands/autoruncommandsWord.js
+++ src/commands/autoruncommandsWord.js
@@ -41,11 +41,27 @@ async function changeHeader(event) {
 
 async function paragraphChanged(event: Word.ParagraphChangedEventArgs) {
   await Word.run(async (context) => {
-    const paragraph = context.document.body.insertParagraph(
-     "${event.type} event detected.",
-      Word.InsertLocation.end
-    );
+    const body = context.document.body;
+    const results = context.document.body.search("110");
+    results.load("length");
     await context.sync();
+    if (results.items.length == 0) {
+      const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+      header.clear();
+      header.insertParagraph("Public - The data is for the public and shareable externally", "Start");
+      const font = header.font;
+      font.color = "#07641d";
+
+      await context.sync();
+    }
+    else {
+      const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+      header.clear();
+      header.insertParagraph("High Confidential - The data must be secret or in some way highly critical", "Start");
+      const font = header.font;
+      font.color = "#f8334d";
+      await context.sync();
+    }
   });
 }
 async function registerOnParagraphChanged(event) {
