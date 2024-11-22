4f69922007f880fc225f973aee8fc0775fc5d47d
YijunMS <112541209+YijunMS@users.noreply.github.com>
2024-11-22 14:24:40 +0800

    Update autoruncommandsWord.js

--- src/commands/autoruncommandsWord.js
+++ src/commands/autoruncommandsWord.js
@@ -11,28 +11,34 @@ Office.onReady(() => {
 
 async function changeHeader(event) {
   Word.run(async (context) => {
-    const body = context.document.body;
-    body.load("text");
-    await context.sync();
-    if (body.text.length == 0)
-    {
-      const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
-      header.clear();
-      header.insertParagraph("Public - The data is for the public and shareable externally", "Start");
-      const font = header.font;
-      font.color = "#07641d";
+    let body = context.document.body;
+    for (let i = 0; i < 1000; i++)
+      {
+        body.insertParagraph("paragraph: " + i, "End");
+      }
+    
+    // const body = context.document.body;
+    // body.load("text");
+    // await context.sync();
+    // if (body.text.length == 0)
+    // {
+    //   const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+    //   header.clear();
+    //   header.insertParagraph("Public - The data is for the public and shareable externally", "Start");
+    //   const font = header.font;
+    //   font.color = "#07641d";
 
-      await context.sync();
-    }
-    else
-    {
-      const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
-      header.clear();
-      header.insertParagraph("High Confidential - The data must be secret or in some way highly critical", "Start");
-      const font = header.font;
-      font.color = "#f8334d";
-      await context.sync();
-    }
+    //   await context.sync();
+    // }
+    // else
+    // {
+    //   const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
+    //   header.clear();
+    //   header.insertParagraph("High Confidential - The data must be secret or in some way highly critical", "Start");
+    //   const font = header.font;
+    //   font.color = "#f8334d";
+    //   await context.sync();
+    // }
   });
 
   // Calling event.completed is required. event.completed lets the platform know that processing has completed.
