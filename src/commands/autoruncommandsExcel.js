44653e12b3d413474fb7f99d44f8332853c6ac5a
Penglong Zhao <penglongzhao@microsoft.com>
2025-10-21 14:41:06 +0800

    test for dialog

--- src/commands/autoruncommandsExcel.js
+++ src/commands/autoruncommandsExcel.js
@@ -43,7 +43,7 @@ async function insertTable(event) {
   }
 
   // Calling event.completed is required. event.completed lets the platform know that processing has completed.
-  event.completed({ allowEvent: true });
+  event.completed({ allowEvent: false });
 }
 
 function getGlobal() {
